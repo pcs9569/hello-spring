355f25c8587dc488cb35a4bf906b07f48afadbb1
pcs9569 <pcs9569@gmail.com>
2021-02-10 16:29:55 +0900

    first commit

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -69,7 +69,7 @@ public class MemberService {
                 });
     }
 
-    //전체 회원 조회회
+    //전체 회원 조회
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
